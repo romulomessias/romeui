2fb9ee694440cc7eb55549e11c88f6b89bb590ce
Romulo Messias <romulomessias@me.com>
2019-05-28 21:57:35 -0300

    some changes

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,9 +1,9 @@
 import React from  'react';
 
-type ButtonProps = React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>
 
 const Button: React.FC<ButtonProps> = (props: ButtonProps ) => {
-    return (<button {...props}> {props.children} </button>)
+    return (<button {...props}> { props.children } </button>)
 };
 
 export default  Button;
